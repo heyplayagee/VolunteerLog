730b487b46aea4e78121271383d2ff478841696e
Stacy Gee <stacy.t.gee@gmail.com>
2015-07-30 01:24:18 -0700

    removed express

--- accounts.js
+++ accounts.js
@@ -1,22 +1,3 @@
-var express = require('express'),
-    app = express(),
-    server = require('http').Server(app),
-    io = require('socket.io')(server),
-    compress = require('compression')(),
-    sqlite3 = require('sqlite3');
-
-app.use(compress);
-app.disable('x-powered-by');
-
-app.use(function (req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8000');
-
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-        next();
-    }
-);
-
 Tabletop.init( { key: '1163y60SPTwj0ikg9YVHTekbSQSTzS_BAxSXgmm648tk',
                    callback: console.log(""),
                    simpleSheet: true } )
@@ -73,6 +54,7 @@ function postContactToGoogle(){
             url: "https://docs.google.com/forms/d/10n6ZtwkB3GCirNvX85FP8_qHTAKaZNIKlh22jOmmTvM/formResponse",
             data: {"entry_1725144764" : username, "entry.2026356525" : password, "entry.1223546042": name},
          	type: "POST",
+         	datatype: "jsonp"
          	datatype: "xml"
          });
 }
