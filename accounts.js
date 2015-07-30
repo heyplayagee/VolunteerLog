04a3614ad7b25ff22329951ecaa2d31ed98eef3f
Stacy Gee <stacy.t.gee@gmail.com>
2015-07-30 01:20:24 -0700

    accounts

--- accounts.js
+++ accounts.js
@@ -1,3 +1,22 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    io = require('socket.io')(server),
+    compress = require('compression')(),
+    sqlite3 = require('sqlite3');
+
+app.use(compress);
+app.disable('x-powered-by');
+
+app.use(function (req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', "http://"+req.headers.host+':8000');
+
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        next();
+    }
+);
+
 Tabletop.init( { key: '1163y60SPTwj0ikg9YVHTekbSQSTzS_BAxSXgmm648tk',
                    callback: console.log(""),
                    simpleSheet: true } )
